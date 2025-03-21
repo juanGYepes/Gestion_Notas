58a27b17cd45aa9c9c2c86472bf12e97c9a79bd1
juanGYepes <juancyepes97@gmail.com>
2025-03-20 23:18:45 -0500

    Funciones de notas

--- Notas.py
+++ Notas.py
@@ -24,4 +24,25 @@ def login():
     print("Usuario o contraseña incorrectos.\n")
     return None
 
+#funcion de agregar notas
+def add_note():
+    print("\n--- Agregar Nota ---")
+    student = input("Ingrese nombre del estudiante: ")
+    subject = input("Ingrese la materia: ")
+    note = float(input("Ingrese la nota: "))
+    grades.append({"estudiante": student, "materia": subject, "nota": note})
+    print("Nota agregada con éxito.\n")
+
+#funcion para ver las notas
+def check_note():
+    print("\n--- Ver Notas ---")
+    student = input("Ingrese su nombre para ver sus notas: ")
+    found = [n for n in grades if n["estudiante"] == student]
+    if found:
+        for n in found:
+            print(f"Materia: {n['materia']}, Nota: {n['nota']}")
+    else:
+        print("No se encontraron notas.\n")
+
+
 
